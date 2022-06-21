7ebfbe0e85d32eeba21dcaaa22273dbff0058272
Santiago <san_chan97@hotmail.com>
2022-06-21 11:26:47 -0300

    update header

--- components/header.js
+++ components/header.js
@@ -1,8 +1,13 @@
-function main() {
-  const logoContainer = document.querySelector(".header__logo-container");
+function addHeader(conteiner) {
+  const headerEl = document.createElement("div");
+  headerEl.innerHTML = ` <header class="header">
+  <div class="header__logo-container"></div>
+
+  <img src="../assets/menu.png  " alt="menu-mobile" class="header__menu" />
+</header>`;
+  const logoContainer = headerEl.querySelector(".header__logo-container");
   addLogo(logoContainer);
   const myLogo = logoContainer.querySelector(".logo");
   myLogo.classList.add("logo--header");
+  conteiner.appendChild(headerEl);
 }
-
-main();
